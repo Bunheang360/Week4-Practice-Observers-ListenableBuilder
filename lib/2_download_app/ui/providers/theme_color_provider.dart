771deb3f55252c6d63d85121b5f0e4e6dc145e90
Bunheang TCO <2005chhengbunheang@gmail.com>
2026-02-22 15:09:24 +0700

    TASK-002-Downloader-Color-Theme

--- lib/2_download_app/ui/providers/theme_color_provider.dart
+++ lib/2_download_app/ui/providers/theme_color_provider.dart
@@ -11,4 +11,15 @@ enum ThemeColor {
   Color get backgroundColor => color.withAlpha(100);
 }
 
-ThemeColor currentThemeColor = ThemeColor.blue;
+final themeColorProvider = ThemeColorProvider();
+
+class ThemeColorProvider extends ChangeNotifier {
+  ThemeColor _currentThemeColor = ThemeColor.blue;
+
+  ThemeColor get currentThemeColor => _currentThemeColor;
+
+  void setThemeColor(ThemeColor themeColor) {
+    _currentThemeColor = themeColor;
+    notifyListeners();
+  }
+}
--- lib/2_download_app/ui/screens/downloads/downloads_screen.dart
+++ lib/2_download_app/ui/screens/downloads/downloads_screen.dart
@@ -23,24 +23,29 @@ class DownloadsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: currentThemeColor.backgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 16),
-          Text(
-            "Downloads",
-            style: AppTextStyles.heading.copyWith(
-              color: currentThemeColor.color,
-            ),
+    return ListenableBuilder(
+      listenable: themeColorProvider,
+      builder: (context,child) {
+        return Container(
+          color: themeColorProvider.currentThemeColor.backgroundColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 16),
+              Text(
+                "Downloads",
+                style: AppTextStyles.heading.copyWith(
+                  color: themeColorProvider.currentThemeColor.color,
+                ),
+              ),
+        
+              SizedBox(height: 50),
+        
+              // TODO - Add the Download tiles
+            ],
           ),
-
-          SizedBox(height: 50),
-
-          // TODO - Add the Download tiles
-        ],
-      ),
+        );
+      }
     );
   }
 }
